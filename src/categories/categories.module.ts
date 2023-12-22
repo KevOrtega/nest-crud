65c3bef7a62ce25450ab0519e1ad91e42619c8bb
KevOrtega <kevortega.dev@gmail.com>
2023-12-21 23:05:44 -0300

    fix(products, categories): entities added in app modules

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoriesController } from './categories.controller';
 import { CategoriesService } from './categories.service';
 import { Category } from './category.entity';
+import { Product } from 'src/products/product.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Category])],
+  imports: [TypeOrmModule.forFeature([Category, Product])],
   controllers: [CategoriesController],
   providers: [CategoriesService],
 })
